344d20c6e456c556b9e0550031be342338123f40
Jeff Kunzelman <jeff@softwareops.com>
2015-03-12 16:17:30 -0700

    Initial Commit

---
+++ PrototypeCellResizing/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PrototypeCellResizing
+//
+//  Created by Jeff Kunzelman on 3/12/15.
+//  Copyright (c) 2015 River.io. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
